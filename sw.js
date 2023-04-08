8b8aff75f536582591b64c259c89800eb121557b
CyberNukes <john.fernandes@westpoint.edu>
2023-04-08 11:50:36 -0400

    Update images and minor tweaks

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
 //Define name of cache and files to cache
-var cachename = 'PWA';
-var filesToCache= ['index.html'];
+const CACHE_NAME = 'PWA_cache';
+const FILES_TO_CACHE= ['/'];
 
-//On install event, cache the specified content from online
-//Fails if fails to add any files
-self.addEventListener('install', function(anEvent)
+//On install event, cache the initial content from online
+//Will fails if it cannot retrive a file
+//synchronous, not robus
+self.addEventListener('install', anEvent =>
 {
     anEvent.waitUntil(
-        cache.open(cacheName).then(function(cache) 
+        caches.open(CACHE_NAME).then(function(cache) 
         {
-            return cache.addAll(filesToCache);
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
 });
 
-//On fetch event, serve fetched content 
-self.addEventListener('fetch', function(anEvent) 
+//On fetch event, serve content if local or retrieve content
+//Synchronous, not robust
+self.addEventListener('fetch', anEvent => 
 {
     anEvent.respondWith(
         caches.match(anEvent).then(function(response) 
